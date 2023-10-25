c4f9cb8d994a79838522fbca84510e7512533f71
Bergan Oudshoorn <bergan404@gmail.com>
2023-10-25 06:49:28 -0400

    gif compressor update

--- Gif_Compressor/image-compress.py
+++ Gif_Compressor/image-compress.py
@@ -1,21 +1,21 @@
 import os
 import subprocess
 
-def compress_gif(input_path, output_path, target_size_MB=5):
+def compress_gif(input_path, output_path, target_size_KB=150):
     try:
-        # Run gifsicle to compress the GIF
-        cmd = f"gifsicle --optimize=3 -o {output_path} {input_path}"
+        # Resize to a smaller dimension and reduce colors aggressively
+        cmd = f"gifsicle --optimize=3 --resize-fit 300x600 --colors 64 --lossy=200 -o {output_path} {input_path}"
         subprocess.run(cmd, shell=True, check=True)
 
         # Check if the resulting file size meets the target size
         current_size = os.path.getsize(output_path)
-        if current_size > target_size_MB * 1024 * 1024:
-            print(f"Compression didn't meet the target size for {input_path} (current size: {current_size / (1024 * 1024):.2f} MB)")
+        if current_size > target_size_KB * 1024:
+            print(f"Compression didn't meet the target size for {input_path} (current size: {current_size / 1024:.2f} KB)")
     except Exception as e:
         # Handle any exceptions that occur during processing
         print(f"Error processing {input_path}: {str(e)}")
 
-def compress_gifs_in_folder(input_folder, output_folder, target_size_MB=5):
+def compress_gifs_in_folder(input_folder, output_folder, target_size_KB=150):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
@@ -23,11 +23,11 @@ def compress_gifs_in_folder(input_folder, output_folder, target_size_MB=5):
         if filename.endswith(".gif"):
             input_file = os.path.join(input_folder, filename)
             output_file = os.path.join(output_folder, filename)
-            compress_gif(input_file, output_file, target_size_MB)
+            compress_gif(input_file, output_file, target_size_KB)
 
 if __name__ == "__main__":
     input_folder = "uncompressed-gif"
     output_folder = "compressed-gif"
-    target_size_MB = 5
+    target_size_KB = 150
 
-    compress_gifs_in_folder(input_folder, output_folder, target_size_MB)
+    compress_gifs_in_folder(input_folder, output_folder, target_size_KB)
